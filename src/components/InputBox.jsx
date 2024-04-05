cdceba23057a0f2af10447c09750f177244cfad9
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2024-04-06 04:13:24 +0530

    Add onChange prop to InputBox component

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,9 +1,10 @@
-export const InputBox = ({ label, placeholder }) => {
+export const InputBox = ({ label, placeholder, onChange  }) => {
     return (
       <div>
         <div className="text-sm font-medium text-left py-2 cursor-pointer">{label}</div>
         <input
           placeholder={placeholder}
+          onChange={onChange}
           className="w-full px-2 py-1 border rounded border-slate-200 hover:border-black focus:border-black focus:outline-none"
         />
       </div>
