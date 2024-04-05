d3364b8e560529f9f8fb6dc1dabc15853f41f252
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2024-04-06 04:19:27 +0530

    Implemented fetching user details in Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { AppBar } from '../components/AppBar';
+import { Balance } from '../components/Balance';
+import { Users } from '../components/Users';
+import axios from "axios";
 
 const Dashboard = () => {
+
+  const userData = localStorage.getItem("user");
+  const user = userData ? JSON.parse(userData) : null;
+  const [balance, setBalance ] = useState(null);
+
+  useEffect(()=>{
+    if(user) {
+      axios.get("http://localhost:4000/api/v1/account/balance", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token") 
+        }
+      })
+      .then(response => {
+        setBalance(response.data.balance);
+      })
+      .catch(error => {
+        console.error('Error fetching balance:', error);
+      });
+    }
+  }, [user]);
+
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <AppBar user={user}/>
+      <div className="h-full w-screen px-24  mt-[-13rem] flex flex-col gap-8">
+        <Balance value={balance} />
+        <Users currentUser={user}/>
+      </div>
+    </div>
   )
 }
 
