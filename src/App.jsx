53456b945fae4bcdae12758b8c7330ba8a4fb7b7
Saurabh Mahapatra <saurabhmahapatra677@gmail.com>
2024-04-06 04:21:33 +0530

    Updated app file

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import SendMoney from './pages/SendMoney';
+import Protected from './components/Protected';
 
 function App() {
+  
   return (
+    
     <div className="bg-[#222831] w-[100vw] h-[100vh] flex items-center justify-center">
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/sendmoney" element={<SendMoney />} />
+          <Route path="/" element={<Protected>  <Dashboard />  </Protected> } />
+          <Route path="/send" element={<Protected> <SendMoney /> </Protected> } />
         </Routes>
       </BrowserRouter>
     </div>
